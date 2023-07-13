e4be3a4da11a130f3498646ed06590272dd79598
OmikM <omik.mueller@gmail.com>
2023-07-13 20:54:31 +0200

    Maching file names. New way to gets elements from soup. works for all profies

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ url = ""
 # path to file must be changed if python script will not be in the same folder as txt files 
 # then it can look like it C:\\Users\\User\\Folder\\file
 
-with open("pkl_data.txt", "r") as file:
+with open("data.txt", "r") as file:
     file = [i for i in file]
     last_pkl = int(file[0])
     last_views = int(file[1])
@@ -18,24 +18,28 @@ print(last_pkl,last_views,last_login)
 
 while True:
     Time = time.strftime("%D:%H:%M:%S", time.localtime())
-    with open("pkl_log.txt", "a") as file:
+    with open("logs.txt", "a") as file:
         file.write("______________________\n"+Time+'\n')
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    b = soup.find_all('b')
 
-    pkl = int(b[6].text)
-    views = int(b[12].text[:-5])
+    td_pkl = soup.find('td', style="font-weight:bold;color:#006934;font-size:24px;text-align:right;padding-right:4px;")
+    a_pkl = td_pkl.find("a").text
+    pkl = int(a_pkl.replace(" ", ""))
+
+    p_views = soup.find('p', style="margin: 8px 0 0; color: gray; text-align: right; font-size: 8px;")
+    b_views = p_views.find("b")
+    views = int(b_views.text[:-5])
 
     if last_pkl != pkl:
-        with open("pkl_log.txt", "a") as file:
+        with open("logs.txt", "a") as file:
             file.write("Now you have"+str(pkl)+"PKLs. you have received"+str(pkl-last_pkl)+"\n")
         last_pkl = pkl
     if views>last_views+1:
-        with open("pkl_log.txt", "a") as file:
+        with open("logs.txt", "a") as file:
             file.write(str(views-(last_views+1))+"People have viewd now its"+str(views)+"\n")
 
-    with open("pkl_data.txt", "w") as file:
+    with open("data.txt", "w") as file:
         file.write(str(pkl)+"\n")
         file.write(str(views)+"\n")
         file.write(Time)
@@ -44,4 +48,5 @@ while True:
     last_login = Time
 
     # time program will wait in secounds before refreshing web 
+    print("yes sr")
     time.sleep(10000)
